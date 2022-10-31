e3cdeb8d56fe7a3880e6717a0b483ed673282c61
Artem <ryaguzov2411@mail.ru>
2022-10-31 23:41:21 +0300

    db dump first try

--- 2.php
+++ 2.php
@@ -146,5 +146,25 @@ function mySortForKey(array &$a, mixed $b)
 }
 ?>
         
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$database = 'test_samson';
+$user = 'root';
+$pass = 'rootpassword';
+$host = 'localhost';
+$dir = dirname(__FILE__) . '/dump.sql';
+
+echo "<h3>Backing up database to `<code>{$dir}</code>`</h3>";
+
+exec("mysqldump --user={$user} --password={$pass} --host={$host} {$database} --result-file={$dir} 2>&1", $output);
+
+var_dump($output);
+#exec('mysqldump --user=root --password= --host=localhost test_samson > /path/to/output/file.sql');
+?>
+        
      </body>
 </html>
